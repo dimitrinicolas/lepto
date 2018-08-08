19709fd83ebcd47ce21b625959fe141c778a3893
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2018-08-08 19:34:53 +0200

    Remove clean-publish temp dir

--- tmph5vXXu/src/lepto.js
+++
@@ -1,8 +0,0 @@
-const plugins = require('./plugins');
-const Runner = require('./runner.js');
-
-const lepto = (options, params) => {
-  return new Runner(options, params);
-};
-
-module.exports = Object.assign(lepto, plugins);
--- tmph5vXXu/src/plugins/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  jpeg: require('./jpeg.js'),
-  png: require('./png.js'),
-  gif: require('./gif.js'),
-  svg: require('./svg.js')
-};
